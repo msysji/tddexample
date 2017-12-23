7a4dbcc15a8b73151b746634e6ebcfa82164b21d
j_ishii <j_ishii@192.168.0.3>
2017-12-23 22:34:08 +0900

    Chapter9

--- WyCash/src/money/Money.java
+++ WyCash/src/money/Money.java
@@ -3,7 +3,13 @@ package money;
 abstract class Money {
 	
 	protected int amount;
+	protected String currency;
 	
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
 	abstract Money times(int multiplier);
 	
 	public boolean equals(Object object) {
@@ -12,11 +18,14 @@ abstract class Money {
 				&& getClass().equals(money.getClass());
 	}
 
+	protected String currency() {
+		return currency;
+	}
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 
 	static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 }
