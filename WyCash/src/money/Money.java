de983d475f0db40c359200e33e76724019fee134
j_ishii <j_ishii@10.4.64.23>
2017-12-24 14:10:12 +0900

    Chapter12

--- WyCash/src/money/Money.java
+++ WyCash/src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression{
 	
 	protected int amount;
 	protected String currency;
@@ -33,5 +33,9 @@ class Money {
 
 	static Money franc(int amount) {
 		return new Money(amount, "CHF");
+	}
+
+	Expression plus(Money addend) {
+		return new Money(amount + addend.amount, currency);
 	}
 }
